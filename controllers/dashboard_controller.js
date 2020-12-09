90a2d5b44c8c6f4c2207453101bf7aaa43d14aec
fadzrinmadu <muhfadzrinmadu@gmail.com>
2020-12-09 15:47:42 +0700

    show top 5 cash in and cash out

--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -2,11 +2,33 @@ const Cash = require('../models/Cash.js');
 const { currencyFormatter } = require('../helpers/formatter.js');
 
 const index = async (req, res) => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+
+  const totalCashInThisMonth = await Cash.getTotalCashInByPeriod(month, year);
+  const totalCashOutThisMonth = await Cash.getTotalCashOutByPeriod(month, year);
+
+  const cashIn = await Cash.find()
+    .where({ type: 'cash-in' })
+    .sort({ amount: 'desc' })
+    .limit(5);
+
+  const cashOut = await Cash.find()
+    .where({ type: 'cash-out' })
+    .sort({ amount: 'desc' })
+    .limit(5);
+
   const data = {
     siteTitle: 'Dashboard',
     totalCashIn: await Cash.getTotalCashIn(),
     totalCashOut: await Cash.getTotalCashOut(),
     totalCash: await Cash.getTotalCash(),
+    totalCashInThisMonth,
+    totalCashOutThisMonth,
+    year,
+    cashIn,
+    cashOut,
     helpers: {
       currencyFormatter,
     },
